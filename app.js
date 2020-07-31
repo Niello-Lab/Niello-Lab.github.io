228db3ab113250a4d8a6ad552d65941e3b9fcccd
Niello-Lab <Niello-Lab@users.noreply.github.com>
2020-07-31 19:14:15 +0200

    Sorting stuff
    Check a few stuff that I had to work on.
    I still struggled but I believe as time progresses I will make edits and updates.
    The due date is 01 Aug 2020.

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 import AudioPlayer from './AudioPlayer.js';
-
-const audioPlayer = new AudioPlayer('.audioPlayer', [
-    { url: 'beats/Broken_Feelings.mp3', name: 'Broken Feelings'},
-    { url: 'beats/Bubble_Trapp.mp3', name: 'Bubble Trap'},
-    { url: 'beats/David_Kramer.mp3', name: 'David Kramer'},
-    { url: 'beats/The_Reaper.mp3', name: 'The Reaper'},
-    { url: 'beats/Lucy.mp3', name: 'Lucy'},
-    { url: 'beats/Muthumbo.mp3', name: 'Muthumbo'},
-    { url: 'beats/Run.mp3', name: 'Run'},
-    { url: 'beats/kasi_Lifestyle.mp3', name: 'Kasi Lifestyle'},
-    { url: 'beats/Still_Kasi.mp3', name: 'Still Kasi'},
-    { url: 'beats/Party.mp3', name: 'Party'},
-    { url: 'beats/Thunder.mp3', name: 'Thunder'},
-    { url: 'beats/Count_on_Me.mp3', name: 'Count on me'},
-    { url: 'beats/No_Mess', name: 'No_Mess'},
-    { url: 'beats/lets_Rap.mp3', name: 'Lets rap'}
+const player = new AudioPlayer('.audioPlayer', [
+    { name: 'Broken Feelings', url: 'beats/Broken_Feelings.mp3'},
+    { name: 'Bubble Trap', url: 'beats/Bubble_Trapp.mp3'},
+    { name: 'David Kramer', url: 'beats/David_Kramer.mp3'},
+    { name: 'The Reaper', url: 'beats/The_Reaper.mp3'},
+    { name: 'Lucy', url: 'beats/Lucy.mp3'},
+    { name: 'Muthumbo', url: 'beats/Muthumbo.mp3'},
+    { name: 'Run', url: 'beats/Run.mp3'},
+    { name: 'Kasi Lifestyle', url: 'beats/kasi_Lifestyle.mp3'},
+    { name: 'Still Kasi', url: 'beats/Still_Kasi.mp3 '},
+    { name: 'Party', url: 'beats/Party.mp3'},
+    { name: 'Thunder', url: 'beats/Thunder.mp3'},
+    { name: 'Count on Me', url: 'beats/Count_on_Me.mp3'},
+    { name: 'No Mess', url: 'beats/No_Mess'},
+    { name: 'Lets Rap', url: 'beats/lets_Rap.mp3'},
 ]);
